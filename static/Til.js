66e68e709eb27acd485c592a24145c8a78dc65a5
The-HansolLee <hansollee333@gmail.com>
2021-09-28 15:20:29 +0900

    Revert "멘트부분 수정"
    This reverts commit c8ecd7c9400ddf43059886ce482317c818c86bc9.

--- static/Til.js
+++ static/Til.js
@@ -54,32 +54,32 @@ function showLocation(position) {   // 위치 정보 호출 성공시
         + "&appid=" + apiKey;
     let options = {method: 'GET'}
     $.ajax(weatherUrl, options).then((response) => {
-        console.log(response) // JSON 타입 위치 및 날씨 정보데이터 로그 확인
+        console.log(response)
         let icon = response.weather[0].icon
         let iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png"
         let img = document.querySelector("#wicon")
         img.src = iconUrl
         let w_icon_id = icon[0]+icon[1];
         if (w_icon_id == '01') {
-            $("#weather_comment").text("맑은 하늘이네요! 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("맑은 하늘이네요! 코딩공부하기 좋은 날~♡");
         } else if (w_icon_id == '02') {
-            $("#weather_comment").text("약간 구름낀 날씨네요! 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("약간 구름낀 날씨네요! 코딩공부하기 좋은 날~♡");
         } else if (w_icon_id == '03') {
-            $("#weather_comment").text("구름이 조금 더 꼈지만 코딩 공부하기 좋은 날씨네요!~♥");
+            $("#weather_comment").text("구름이 조금 더 꼈지만 코딩 공부하기 좋은 날씨네요!");
         } else if (w_icon_id == '04') {
-            $("#weather_comment").text("구름이 좀 끼고 우중충하니까 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("구름이 좀 끼고 우중충하니까 코딩공부하기 좋은 날~♡");
         } else if (w_icon_id == '09') {
-            $("#weather_comment").text("소나기가 내리는 지금은 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("소나기가 내리는 지금은 코딩공부하기 좋은 날~♡");
         } else if (w_icon_id == '10') {
-            $("#weather_comment").text("비가 와요! 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("비가 와요! 코딩공부하기 좋은 날~♡");
         } else if (w_icon_id == '11') {
-            $("#weather_comment").text("천둥 번개가 치는 지금은?! 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("천둥 번개가 치는 지금은?! 코딩공부하기 좋은 날~♡");
         } else if (w_icon_id == '13') {
-            $("#weather_comment").text("눈이 와요~! 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("눈이 와요~! 코딩공부하기 좋은 날~♡");
         } else
-            $("#weather_comment").text("안개가 낀 날씨도 역시! 코딩공부하기 좋은 날~♥");
+            $("#weather_comment").text("안개가 낀 날씨도 역시! 코딩공부하기 좋은 날~♡");
 
-        $("#tempr").text(Math.round(parseFloat(response.main.temp - 273)) + '˚'); // 현재 온도
+        $("#tempr").text(parseInt(response.main.temp - 273) + '˚'); // 현재 온도
     }).catch((error) => {
         console.log(error)
     })
