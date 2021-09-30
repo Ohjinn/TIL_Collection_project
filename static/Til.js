87ce3a883930b5739ed96abde663222b81e1ea0c
The-HansolLee <hansollee333@gmail.com>
2021-10-01 01:19:03 +0900

    NewRALPH

--- static/Til.js
+++ static/Til.js
@@ -38,11 +38,11 @@ function getCards() {
     })
 }
 
-
 function velClick() {
     if ($("#velog-box").is(":visible")) {
         $("#velog-box").hide();
-    } else { $("#velog-box").show();
+    } else {
+        $("#velog-box").show();
     }
 }
 
@@ -111,7 +111,7 @@ function makeVelogCard(cards){
                             <p class="card-text">${cards['url']}</p>
                             <div class="d-flex justify-content-center">
                             <a href="#" onclick="window.open('${cards['url']}', 'new')" class="btn btn-warning st">바로가기</a>
-                            <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-warning st">리뷰달기</button>
+<!--                            <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-warning st">리뷰달기</button>-->
                         </div>
                         </div>
                     </div>`
@@ -126,13 +126,12 @@ function makeTistoryCard(cards){
                             <p class="card-text">${cards['url']}</p>
                             <div class="d-flex justify-content-center">
                             <a href="#" onclick="window.open('${cards['url']}', 'new')" class="btn btn-warning st">바로가기</a>
-                            <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-warning st">리뷰달기</button>
+<!--                            <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-warning st">리뷰달기</button>-->
                         </div>
                         </div>
                     </div>`
     $("#tistory-box").append(tempHtml);
 }
-
 
 //검색
 function search() {
@@ -143,17 +142,24 @@ function search() {
         data: {},
         success: function (response) {
             $("#flush").empty();
-
+            let tempHtml =``
             if (txt == response.name) {
                 let tempHtml = `<div class="card hotboxs">
-                        <img class="card-img-top card-rows" src="${response['pic']}" alt="Card image cap">
+                        <img class="card-img-top card-rows" height="200" src="${response['pic']}" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">${response['name']}</h5>
                             <p class="card-text">${response['url']}</p>
-                            <a href="${response['url']}" class="btn btn-dark">바로가기</a>
+                            <div class="d-flex justify-content-center">
+                            <a href="${response['url']}" class="btn btn-warning st">바로가기</a>
+<!--                            <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-warning st">리뷰달기</button>-->
+                        </div>
                         </div>
                     </div>
-                    <button onclick="window.location.href = '/'" type="button">메인으로</button>`
+                    <button onclick="window.location.href = '/'" type="button" class="btn btn-primary ">메인으로</button>`
+                $("#flush").append(tempHtml);
+            }
+            else {
+                let tempHtml =`<button onclick="window.location.href = '/'" type="button" class="btn btn-primary">메인으로</button>`
                 $("#flush").append(tempHtml);
             }
         }
